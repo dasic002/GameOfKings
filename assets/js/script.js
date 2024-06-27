4ce38acf7719046946f581a69e3e42d783c479f8
Carlos Da Silva <dasic002@yahoo.co.uk>
2024-06-27 10:20:41 +0000

    Update code to capture player details from form
    Add logbot function to assume bot if field empty
    
    Add basic code to proceed to game setup
    
    Add function to track player and dealer turns

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,19 @@
-// Constant for gameData()
-const playerData = {};
+// Constant for player data to manipulate throughout the game
+const playerData = [];
+// Constant to keep track of whose turn it is
+let turnIndex = 1;
+// Constant to keep track of who took a 'turn of dealing the cards', will always assume Player deals first on new game
+let dealerIndex = 0;
+// Constant for tracking if a game has more than 1 human player, so it triggers prompts between before a human player's turn
+let humans = false;
+// Constant for tracking the draw deck of cards
+const drawDeck = [];
+// Constant for tracking the discarded deck of cards
+const discardDeck = [];
 
 // wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 // based on DOM loading event listener from Love Maths walkthrough project
-
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByClassName("click"); // event listener will look clickable elements by the class "click"
 
@@ -25,25 +34,45 @@ document.addEventListener("DOMContentLoaded", function () {
                 document.getElementById('player-form').style.display = 'block';
             } else if (this.getAttribute('action') === 'next') {
                 if(this.innerText === "Start Game"){
-                    let P1 = [String(document.getElementById('P1-name').value), 0];
-                    let P2 = [String(document.getElementById('P2-name').value)];
-                    let P3 = [String(document.getElementById('P2-name').value)];
-                    let P4 = [String(document.getElementById('P2-name').value)];
-                    // P1.push(0);
-                    // addBot(P2)
+                    let P1 = String(document.getElementById('P1-name').value);
+                    let P2 = String(document.getElementById('P2-name').value);
+                    let P3 = String(document.getElementById('P3-name').value);
+                    let P4 = String(document.getElementById('P4-name').value);
+                    P1 === '' ? P1 = ['Player 1', 0] : P1 = [P1, 0];
+                    P2 === '' ? P2 = ['_BOT 2', 2] : P2 = logBot('P2', P2);
+                    P3 === '' ? P3 = ['_BOT 3', 2] : P3 = logBot('P3', P3);
+                    P4 === '' ? P4 = ['_BOT 4', 2] : P4 = logBot('P4', P4);
+                    // console.log(P1);
                     // console.log(P2);
-                    // console.log(P2[0].includes('_BOT 2'));
-                    if (P2[0].includes('_BOT 2') === true){
-                        let x = P2[0].indexOf(',');
-                        // console.log(x);
-                        if ( x > 0){
-                            x = parseInt(P2[0].slice(x + 1, x + 2));
-                            // console.log(x);
-                        }
-                        P2 = ['_BOT 2', x];
+                    // console.log(P3);
+                    // console.log(P4);
+                    P1 = {
+                        botSkill: P1[1],
+                        playerName: P1[0],
+                        cardHand: [],
+                        score: 0
                     }
-                    console.log(P2);
-                    alert(P2);
+                    P2 = {
+                        botSkill: P2[1],
+                        playerName: P2[0],
+                        cardHand: [],
+                        score: 0
+                    }
+                    P3 = {
+                        botSkill: P3[1],
+                        playerName: P3[0],
+                        cardHand: [],
+                        score: 0
+                    }
+                    P4 = {
+                        botSkill: P4[1],
+                        playerName: P4[0],
+                        cardHand: [],
+                        score: 0
+                    }
+                    playerData.push(P1, P2, P3, P4);
+                    turnIndex = dealerIndex + 1;
+                    runGame('new',);
                 }
             } else {
                 let item = this.getAttribute('id') === null ? this.innerText : this.getAttribute('id');
@@ -75,10 +104,28 @@ function menu() {
 }
 
 // function that will manipulate data stored in constants as the game goes on
-function gameData(action, data){
+function runGame(action, data){
     if (action === 'new'){
-
+        table('empty');
+        document.getElementById('player-form').style.display = 'none';
+        document.getElementById('other-players').style.display = 'block';
+        document.getElementById('decks-area').style.display = 'block';
+        document.getElementById('main-player').style.display = 'block';
     }
+}
+
+// manages appearance of cards on the table 
+// 'empty' will display a table with only placeholders for card
+// 'deal' will add a card at a time to the table to mimic dealing out
+function table(action){
+    if(action === 'empty'){
+        alert('will add class "missing" to all card places on the table');
+    }
+}
+
+// will manipulate the cards held in the draw deck, including dealing at the start of the game
+// action of 'deal' will set the draw deck as a fresh deck of cards, shuffle it and return the first 17 cards for players and discard deck
+function drawStack(action, data){
 }
 
 // checks that the string contains the term and removes the term
@@ -97,4 +144,33 @@ function addClass(strg1, term){
         strg1 += term;
     }
     return strg1;
+}
+
+// Captures the data for bot players, including the skill level, and presumes standard level 2 bot if the fields are left empty
+function logBot (Pn, strg1){
+    let x;
+    let y;
+    if (strg1.includes('_BOT ') === true){
+        x = strg1.indexOf(',');
+        // console.log(x);
+        if ( x > 0){
+            y = strg1.slice(0, x);
+            x = parseInt(strg1.slice(x + 1, x + 2));
+            // console.log(x);
+            
+        } else {
+            x = 2;
+        }
+        return strg1 = [y, x];
+    } else {
+        return strg1 = [strg1, 0];
+    }
+}
+
+// shifts turns for either play turn or dealer turn. Action must be 'dealer', otherwise assumes play turn is being shifted.
+function shiftTurns (action){
+    let x;
+    action === 'dealer' ? x = dealerIndex : x = turnIndex;
+    x > 2 ? x = 0 : x++;
+    action === 'dealer' ? dealerIndex = x : turnIndex = x;
 }
