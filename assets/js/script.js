3cc7f886fe190aa48205ca121247f92b1f9252de
Carlos Da Silva <dasic002@yahoo.co.uk>
2024-07-01 18:55:49 +0000

    Add code for prompting human players inc support
    Fix player name placing to account for turnIndex
    
    Add code for tracking round to apply right prompt
    
    Add class click to HTML for detection by listener
    
    Fix spelling of bot names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 // Constant for player data to manipulate throughout the game
 const playerData = [];
 // Constant to keep track of whose turn it is
-let turnIndex = 1;
+let turnIndex = 0;
 // Constant to keep track of who took a 'turn of dealing the cards', will always assume Player deals first on new game
 let dealerIndex = 0;
+let oldDealerIndex = 0;
 // Constant for tracking if a game has more than 1 human player, so it triggers prompts between before a human player's turn
 let humans = false;
+// Constant to countdown turns from the start of a new round to enable the appropriate player prompt to display
+let newRound = 0;
 // Constant for tracking the draw deck of cards
 const drawDeck = [];
 // Constant for tracking the discarded deck of cards
@@ -19,9 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     for (let button of buttons) {
         button.addEventListener('click', function () {
-            if (this.getAttribute('action') === 'submit') {
-                alert('this element has an action attr of "submit"');
-            } else if (this.getAttribute('action') === 'menu') {
+            if (this.getAttribute('action') === 'menu') {
                 menu();
             } else if (this.getAttribute('action') === 'how-to-play') {
                 menu();
@@ -29,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 let state = this.innerText;
                 state === 'OFF' ? state = 'ON' : state = 'OFF';
                 this.innerHTML = state;
-            } else if (this.getAttribute('id') === 'play') {
+            } else if (this.getAttribute('action') === 'play') {
                 document.getElementById('welcome').style.display = 'none';
                 document.getElementById('player-form').style.display = 'block';
             } else if (this.getAttribute('action') === 'next') {
@@ -39,13 +40,9 @@ document.addEventListener("DOMContentLoaded", function () {
                     let P3 = String(document.getElementById('P3-name').value);
                     let P4 = String(document.getElementById('P4-name').value);
                     P1 === '' ? P1 = ['Player 1', 0] : P1 = [P1, 0];
-                    P2 === '' ? P2 = ['_BOT 2', 2] : P2 = logBot(P2);
-                    P3 === '' ? P3 = ['_BOT 3', 2] : P3 = logBot(P3);
-                    P4 === '' ? P4 = ['_BOT 4', 2] : P4 = logBot(P4);
-                    // console.log(P1);
-                    // console.log(P2);
-                    // console.log(P3);
-                    // console.log(P4);
+                    P2 === '' ? P2 = ['_BOT 1', 2] : P2 = logBot(P2);
+                    P3 === '' ? P3 = ['_BOT 2', 2] : P3 = logBot(P3);
+                    P4 === '' ? P4 = ['_BOT 3', 2] : P4 = logBot(P4);
                     P1 = {
                         botSkill: P1[1],
                         playerName: P1[0],
@@ -71,9 +68,24 @@ document.addEventListener("DOMContentLoaded", function () {
                         score: 0
                     }
                     playerData.push(P1, P2, P3, P4);
-                    turnIndex = dealerIndex + 1;
                     runGame('new');
                 }
+            } else if (this.getAttribute('action') === 'new-hand') {
+                hand('open');
+                newRound--;
+            } else if (this.getAttribute('action') === 'done') {
+                if (newRound > 0) {
+                    shiftTurns();
+                    plrPrompt();
+                } else {
+                    hand('close');
+                    shiftTurns();
+                    if (humans === true) {
+                        plrPrompt();
+                    }
+                }
+
+
             } else {
                 let item = this.getAttribute('id') === null ? this.innerText : this.getAttribute('id');
                 alert(item);
@@ -107,14 +119,14 @@ function menu() {
 // function that will run the sequence of functions of the game, triggering functions to manipulate data stored in constants
 function runGame(action, data) {
     if (action === 'new') {
+        table("playerNames");
         table('empty');
         document.getElementById('player-form').style.display = 'none';
         document.getElementById('other-players').style.display = 'block';
         document.getElementById('decks-area').style.display = 'block';
         document.getElementById('main-player').style.display = 'block';
         drawStack('deal');
-        
-
+        plrPrompt();
     }
 }
 
@@ -122,6 +134,8 @@ function runGame(action, data) {
 // 'empty' will display a table with only placeholders for card
 // 'deal' will add player names, scores and a card at a time to the table to mimic dealing out
 // 'values' will return the card ID as inner HTML text in the card location
+// 'playerNames' looks up the turnIndex and places names and scores in the correct position depending on whose turn it is, 
+// so the current player will always be in the bottom card hand..
 function table(action, data) {
     let cards;
     let decks;
@@ -139,17 +153,6 @@ function table(action, data) {
     let p4Score = document.getElementById('scr4');
 
     if (action === 'empty') {
-        // display players names
-        p1Name.innerHTML = playerData[0].playerName;
-        p2Name.innerHTML = playerData[1].playerName;
-        p3Name.innerHTML = playerData[2].playerName;
-        p4Name.innerHTML = playerData[3].playerName;
-        // display players current scores
-        p1Score.innerHTML = playerData[0].score;
-        p2Score.innerHTML = playerData[1].score;
-        p3Score.innerHTML = playerData[2].score;
-        p4Score.innerHTML = playerData[3].score;
-
         for (let card of cards) {
             let x = card.getAttribute('class');
             x = addClass(x, ' missing-card');
@@ -163,12 +166,12 @@ function table(action, data) {
             card.innerHTML = '';
         }
     } else if (action === "deal") {
-        // console.log(data);
-        if(data < 16){
+        console.log(data);
+        if (data < 16) {
             let y = cards[data].getAttribute('class');
             y = remClass(y, 'missing-card');
             cards[data].setAttribute('class', y);
-        } else if(data < 22) {
+        } else if (data < 22) {
             data = data - 16;
             let y = decks[data].getAttribute('class');
             y = remClass(y, 'missing-card');
@@ -176,10 +179,10 @@ function table(action, data) {
         } else {
             alert('action "deal" given data larger than 21!');
         }
-        
-    } else if (action ==='values') {
-        if(data < 16){
-            cards[data].innerHTML = playerData[Math.floor(data / 4)].cardHand[data % 4];
+
+    } else if (action === 'values') {
+        if (data < 16) {
+            cards[data].innerHTML = playerData[turnIndex].cardHand[data % 4];
         } else if (data < 19) {
             data = data - 16;
             let i = data + 1;
@@ -191,7 +194,35 @@ function table(action, data) {
         } else {
             alert('action "values" given data larger than 21!');
         }
-            
+
+    } else if (action === 'playerNames') {
+        let x = turnIndex - 1;
+        for (let i = 0; i < 4; i++) {
+            x > 2 ? x = 0 : x++;
+            switch (i) {
+                case (0):
+                    p1Name.innerHTML = playerData[x].playerName; // display player names
+                    p1Score.innerHTML = playerData[x].score; // display player current score
+                    continue;
+                case (1):
+                    p2Name.innerHTML = playerData[x].playerName;
+                    p2Score.innerHTML = playerData[x].score;
+                    continue;
+                case (2):
+                    p3Name.innerHTML = playerData[x].playerName;
+                    p3Score.innerHTML = playerData[x].score;
+                    continue;
+                case (3):
+                    p4Name.innerHTML = playerData[x].playerName;
+                    p4Score.innerHTML = playerData[x].score;
+            }
+        }
+
+    } else if (action === 'focus') {
+        table('playerNames');
+        for (let i = 0; i < 16; i++) {
+            table('values', i);
+        }
     }
 }
 
@@ -212,43 +243,145 @@ function drawStack(action, data) {
             drawDeck.push(freshDeck[i]);
         }
         console.log(drawDeck);
+        if (dealerIndex === oldDealerIndex) {
+            shiftTurns('dealer');
+            turnIndex = dealerIndex;
+            newRound = 4;
+        }
         for (let i = 0; i < 4; i++) { // iterate through card hand position
             for (let j = 0; j < 4; j++) { // iterate through players, so deals card 1 to all players, before card 2
                 card = drawDeck.pop();
-                playerData[j].cardHand.push(card);
-                // console.log(playerData[j].cardHand[i]);
+                playerData[turnIndex].cardHand.push(card);
                 let place = i + 4 * j;
                 table('deal', place);
                 table('values', place);
+                shiftTurns();
             }
         }
         card = drawDeck.pop();
         discardStack('add', card);
         // now that the drawDeck array is as it will be to start the game, finish displaying the table setup by displaying 3 cards in discard stack
-        for(let i = 0; i < 3; i++) {
+        for (let i = 0; i < 3; i++) {
             table('deal', 16 + i);
             table('values', 16 + i);
         }
-        
+
     }
 }
 
 // will manipulate cards present in the discard stack
 // 'add' will add the card ID as a string value forwarded as the data
-function discardStack(action, data){
-    if(action === 'add') {
+function discardStack(action, data) {
+    if (action === 'add') {
         discardDeck.push(data);
         let x = discardDeck.length;
         x = Math.min(x, 3);
         console.log(`number of cards in discard deck = ${x}`);
         // Run through the length of discard stack up to its top 3 cards to illustrate them on the table
-        for(let i = 0; i < x; i++) { 
+        for (let i = 0; i < x; i++) {
             table('deal', 19 + i);
             table('values', 19 + i);
         }
-        
+
     }
 }
+
+// function will produce a prompt for human players when more than one human is playing on the same device
+function plrPrompt() {
+    if (newRound > 0) {
+        if (newRound === 1) {
+            console.log('last prompt for new hand!');
+        }
+
+        if (humans === true && playerData[turnIndex].botSkill === 0) {
+            console.log(`${playerData[turnIndex].playerName} is a human player!`);
+            document.getElementById('wlcm-msg').innerHTML = `${playerData[turnIndex].playerName}`;
+            document.getElementById('play').innerHTML = 'READY';
+            document.getElementById('play').setAttribute('action', 'new-hand');
+            document.getElementById('welcome').style.display = 'block';
+            document.getElementById('other-players').style.display = 'none';
+            document.getElementById('decks-area').style.display = 'none';
+            document.getElementById('main-player').style.display = 'none';
+            //newRound--;
+        } else if (humans === false) {
+            console.log('no humans but P1!')
+            turnIndex = 0;
+            newRound = 1;
+            hand('open');
+            newRound--;
+        } else {
+            console.log(`bot player - turnIndex: ${turnIndex}`)
+            shiftTurns();
+            newRound--;
+            plrPrompt();
+        }
+
+    } else {
+        if (humans === true && playerData[turnIndex].botSkill === 0) {
+            console.log(`${playerData[turnIndex].playerName} is a human player!`);
+            document.getElementById('wlcm-msg').innerHTML = `${playerData[turnIndex].playerName}!<br>Pick a card!`;
+            document.getElementById('play').innerHTML = 'READY';
+            document.getElementById('play').setAttribute('action', 'to-pick');
+            document.getElementById('welcome').style.display = 'block';
+            document.getElementById('other-players').style.display = 'none';
+            document.getElementById('decks-area').style.display = 'none';
+            document.getElementById('main-player').style.display = 'none';
+        } else if (turnIndex === 0) {
+            // ; // --- perhaps just code to highlight main player area to indicate turn
+        } else {
+            // ; // --- code to highlight bot currently playing;
+            // ; // --- code to get bot to make a decision on play;
+            // shiftTurns();
+        }
+    }
+}
+
+// presents current player with hand of cards, 
+// 'open' will give the player a chance to swap card positions before the game begins
+// 'close' will go back to table view
+function hand(action, data) {
+    document.getElementById('welcome').style.display = 'none';
+    document.getElementById('other-players').style.display = 'none';
+    document.getElementById('decks-area').style.display = 'none';
+    document.getElementById('main-player').style.display = 'block';
+    // selects the 4 cards in the 'main-player' div only
+    let cards = document.getElementById('main-player').getElementsByClassName('card');
+
+    if (action === 'open') {
+        // adds class of deck so appearance is larger as set for the cards in the decks
+        for (let card of cards) {
+            let x = card.getAttribute('class');
+            x = addClass(x, ' deck');
+            card.setAttribute('class', x);
+        }
+        // for testing, sets the cards ID to the correct position in the player's hand
+        for (let i = 0; i < 4; i++) {
+            cards[i].innerHTML = playerData[turnIndex].cardHand[i];
+        }
+        // presents main-player name, score and message
+        document.getElementById('p1').innerHTML = playerData[turnIndex].playerName;
+        document.getElementById('scr1').innerHTML = playerData[turnIndex].score;
+        if (newRound >= 0) {
+            document.getElementById('top-prompt').innerHTML = 'Want to shuffle?';
+        } else {
+            document.getElementById('top-prompt').innerHTML = 'Pick a card to swap';
+        }
+        
+
+    } else if (action === 'close') {
+        for (let card of cards) {
+            let x = card.getAttribute('class');
+            x = remClass(x, ' deck');
+            card.setAttribute('class', x);
+        }
+        document.getElementById('top-prompt').style.display = 'none';
+        document.getElementById('btm-prompt').style.display = 'none';
+        document.getElementById('other-players').style.display = 'block';
+        document.getElementById('decks-area').style.display = 'block';
+        document.getElementById('main-player').style.display = 'block';
+    }
+}
+
 
 // checks that the string contains the term and removes the term
 function remClass(strg1, term) {
@@ -276,15 +409,16 @@ function logBot(strg1) {
         x = strg1.indexOf(',');
         // console.log(x);
         if (x > 0) {
-            y = strg1.slice(0, x);  // separate the bot name '_BOT n'
-            x = parseInt(strg1.slice(x + 1, x + 2));  // get number for skill level after comma
+            y = strg1.slice(0, x); // separate the bot name '_BOT n'
+            x = parseInt(strg1.slice(x + 1, x + 2)); // get number for skill level after comma
             // console.log(x);
 
         } else {
-            x = 2;  // else assume skill level of 2
+            x = 2; // else assume skill level of 2
         }
         return strg1 = [y, x];
     } else {
+        humans = true;
         return strg1 = [strg1, 0]; // if does not contain '_BOT ' then assume the name is human and bot skill level is set to 0
     }
 }
